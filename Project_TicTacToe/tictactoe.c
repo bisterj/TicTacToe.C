3a56c2874994da132f401a468651eabed96bf9af
bisterj <69654276+bisterj@users.noreply.github.com>
2025-02-14 05:00:48 -0600

    created smart ai which will first look for win, then block win, and if neither of those available it chooses random. Still not working 100%

--- Project_TicTacToe/tictactoe.c
+++ Project_TicTacToe/tictactoe.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+int main(){{
 	// seeding the random thingy
 	srand(time(NULL));
 	// declaration for function that looks for wins
@@ -13,6 +13,8 @@ int main(){
 	void dumb_ai(char board[]);
 
 	void rand_ai(char board[]);
+
+	void smart_ai(char board[]);
 	
 	// This is the game board. Will have x's and o's instead of spaces as time goes on
 	char board[] = " | | \n_____\n | | \n_____\n | | ";
@@ -31,38 +33,46 @@ int main(){
 	int move[2];
 	int turn = 0;
 	// Game Loop...
-	while (test_win(board) == 1){
-	printf("%s\n", board);
-	if (turn == 0){
-	printf("It is x's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
-	scanf("%d %d", &move[0], &move[1]);
-	make_move(turn, board, move);
-	} else {
-	printf("AI moving...\n");
-	dumb_ai(board);
+	while (test_win(board) == 1 && turn < 9){
+		printf("%s\n", board);
+		if (turn % 2 == 0){
+			printf("It is x's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
+			scanf("%d %d", &move[0], &move[1]);
+			make_move(turn, board, move);
+		} else {
+			printf("AI moving...\n");
+			smart_ai(board);
+		}
+		
+		/*else {
+			printf("It is o's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
+		}*/
+		
+		turn++;
+		}
+		
+		// Text will be printed if you won.
+		printf("%s\n", board);
+		if(test_win(board) == 0){
+		printf("Yay, you won!");
+		} else {
+			printf("Tie... BORING!");
+		}
 	}
-	
-	/*else {
-	printf("It is o's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
-	}*/
-	
-	turn = (turn + 1) % 2;
-	}
-	
-	// Text will be printed if you won.
-	printf("%s\n", board);
-	if(test_win(board) == 0){
-	printf("Yay, you won!");
-	};
 	
 
 	// End of main function.
 	return 0;
 }
 
+int ints_to_index(int move0, int move1){
+	int index = move0 * 12 + move1 * 2;
+	return index;
+}
+
 void dumb_ai(char board[]){
 	int move[2];
-	//look for first open position
+	// Look for first open position
 	for (int r = 2; r>-1; r--){
 		for (int c = 0; c<3; c++){
 			if (board[r*12 + c*2] == ' '){
@@ -91,9 +101,144 @@ void rand_ai(char board[]){
 	board[index] = 'o';
 }
 
+void smart_ai(char board[]){
+	int ints_to_index(int move0, int move1);
+	
+	// Those easy indices again...
+	int top_left = 0;
+	int top_mid = 2;
+	int top_right = 4;
+	int mid_left = 12;
+	int mid_mid = 14;
+	int mid_right = 16;
+	int bot_left = 24;
+	int bot_mid = 26;
+	int bot_right = 28;
+	
+	int move[2];
+	int index = 0;
+	move[0] = -1;
+	// Check for win on this move
+	// All ways to win top left
+	if (board[top_left] == 'o'){
+		// Top Horizontal
+		if(board[top_mid] == 'o' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+		else if(board[top_right] == 'o' && board[ints_to_index(0, 1)] == ' '){move[0] = 0; move[1] = 1;}
+		// Top Vertical
+		else if(board[mid_left] == 'o' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+		else if(board[bot_left] == 'o' && board[ints_to_index(1, 0)] == ' '){move[0] = 1; move[1] = 0;}
+		// Top Left Diagonal
+		else if(board[mid_mid] == 'o' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'o' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+	// All new ways to check top mid
+	} else if(board[top_mid] == 'o'){
+		if(board[mid_mid] == 'o' && board[ints_to_index(2, 1)] == ' '){move[0] = 2; move[1] = 1;}
+		else if(board[bot_mid] == 'o' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+	// All new ways to check top right
+	} else if(board[top_right] == 'o'){
+		//Top Right Horizontal
+		if(board[top_mid] == 'o' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+		// Top Right Diagonal
+		else if(board[mid_mid] == 'o' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+		else if(board[bot_left] == 'o' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+		// Top Right Vertical
+		else if(board[mid_right] == 'o' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'o' && board[ints_to_index(1, 2)] == ' '){move[0] = 1; move[1] = 2;}
+	// All new ways to check mid left
+	} else if(board[mid_left] == 'o'){
+		if(board[mid_mid] == 'o' && board[ints_to_index(1, 2)] == ' '){move[0] = 1; move[1] = 2;}
+		else if(board[mid_right] == 'o' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+		else if(board[bot_left] == 'o' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+	// All new ways to check mid mid
+	} else if(board[mid_mid] == 'o'){
+		if(board[mid_right] == 'o' && board[ints_to_index(1, 0)] == ' '){move[0] = 1; move[1] = 0;}
+		else if(board[bot_mid] == 'o' && board[ints_to_index(0, 1)] == ' '){move[0] = 0; move[1] = 1;}
+	// All new ways to check mid right
+	} else if(board[mid_right] == 'o'){
+		if(board[bot_right] == 'o' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+	// All new ways to check bot left
+	} else if(board[bot_left] == 'o'){
+		if(board[mid_mid] == 'o' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+		else if(board[bot_mid] == 'o' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'o' && board[ints_to_index(2, 1)] == ' '){move[0] = 2; move[1] = 1;}
+	// All new ways to check bot mid
+	} else if(board[bot_mid] == 'o'){
+		if(board[bot_right] == 'o' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+	// All new ways to check bot right
+	} else if(board[bot_right] == 'o'){
+		if(board[mid_mid] == 'o' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+	}
+	// ELSE Check for win for opp on next move
+	if (move[0] == -1){
+		if (board[top_left] == 'x'){
+		// Top Horizontal
+		if(board[top_mid] == 'x' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+		else if(board[top_right] == 'x' && board[ints_to_index(1, 1)] == ' '){move[0] = 0; move[1] = 1;}
+		// Top Vertical
+		else if(board[mid_left] == 'x' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+		else if(board[bot_left] == 'x' && board[ints_to_index(1, 0)] == ' '){move[0] = 1; move[1] = 0;}
+		// Top Left Diagonal
+		else if(board[mid_mid] == 'x' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'x' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+	// All new ways to check top mid
+	} else if(board[top_mid] == 'x'){
+		if(board[mid_mid] == 'x' && board[ints_to_index(2, 1)] == ' '){move[0] = 2; move[1] = 1;}
+		else if(board[bot_mid] == 'x' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+	// All new ways to check top right
+	} else if(board[top_right] == 'x'){
+		//Top Right Horizontal
+		if(board[top_mid] == 'x' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+		// Top Right Diagonal
+		else if(board[mid_mid] == 'x' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+		else if(board[bot_left] == 'x' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+		// Top Right Vertical
+		else if(board[mid_right] == 'x' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'x' && board[ints_to_index(1, 2)] == ' '){move[0] = 1; move[1] = 2;}
+	// All new ways to check mid left
+	} else if(board[mid_left] == 'x'){
+		if(board[mid_mid] == 'x' && board[ints_to_index(1, 2)] == ' '){move[0] = 1; move[1] = 2;}
+		else if(board[mid_right] == 'x' && board[ints_to_index(1, 1)] == ' '){move[0] = 1; move[1] = 1;}
+		else if(board[bot_left] == 'x' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+	// All new ways to check mid mid
+	} else if(board[mid_mid] == 'x'){
+		if(board[mid_right] == 'x' && board[ints_to_index(1, 0)] == ' '){move[0] = 1; move[1] = 0;}
+		else if(board[bot_mid] == 'x' && board[ints_to_index(0, 1)] == ' '){move[0] = 0; move[1] = 1;}
+	// All new ways to check mid right
+	} else if(board[mid_right] == 'x'){
+		if(board[bot_right] == 'x' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+	// All new ways to check bot left
+	} else if(board[bot_left] == 'x'){
+		if(board[mid_mid] == 'x' && board[ints_to_index(0, 2)] == ' '){move[0] = 0; move[1] = 2;}
+		else if(board[bot_mid] == 'x' && board[ints_to_index(2, 2)] == ' '){move[0] = 2; move[1] = 2;}
+		else if(board[bot_right] == 'x' && board[ints_to_index(2, 1)] == ' '){move[0] = 2; move[1] = 1;}
+	// All new ways to check bot mid
+	} else if(board[bot_mid] == 'x'){
+		if(board[bot_right] == 'x' && board[ints_to_index(2, 0)] == ' '){move[0] = 2; move[1] = 0;}
+	// All new ways to check bot right
+	} else if(board[bot_right] == 'x'){
+		if(board[mid_mid] == 'x' && board[ints_to_index(0, 0)] == ' '){move[0] = 0; move[1] = 0;}
+	}
+	}
+	// ELSE Random move 
+	if(move[0] == -1){
+		move[0] = rand() % 3;
+		move[1] = rand() % 3;
+		index = move[0] * 12 + move[1] * 2;
+		while(board[index] != ' '){
+			move[0] = rand() % 3;
+			move[1] = rand() % 3;
+			index = move[0] * 12 + move[1] * 2;
+		}
+	}
+	
+	// Make the move
+	index = 12 * move[0] + 2 * move[1];
+	board[index] = 'o';
+}
+
 void make_move(int turn, char board[], int move[]){
 	int index = move[0] * 12 + move[1] * 2;
-	if (turn == 0){
+	if (turn % 2 == 0){
 	board[index] = 'x';
 	} else {
 	board[index] = 'o';
