48b4baf940743c0272cb071d29dd01dfe5de7fb2
bisterj <69654276+bisterj@users.noreply.github.com>
2025-02-14 01:53:15 -0600

    Made game loop more substantial and take turns, added two basic bots to play against dumb_ai which just chooses the next possible move sequentially on the board and rand_ai which chooses a random location that is available.

--- Project_TicTacToe/tictactoe.c
+++ Project_TicTacToe/tictactoe.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
+	// seeding the random thingy
+	srand(time(NULL));
 	// declaration for function that looks for wins
 	int test_win(char board[]);
 
 	void make_move(int turn, char board[], int move[]);
+
+	void dumb_ai(char board[]);
+
+	void rand_ai(char board[]);
 	
 	// This is the game board. Will have x's and o's instead of spaces as time goes on
 	char board[] = " | | \n_____\n | | \n_____\n | | ";
@@ -22,12 +30,23 @@ int main(){
 	
 	int move[2];
 	int turn = 0;
-	
+	// Game Loop...
 	while (test_win(board) == 1){
 	printf("%s\n", board);
+	if (turn == 0){
 	printf("It is x's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
 	scanf("%d %d", &move[0], &move[1]);
 	make_move(turn, board, move);
+	} else {
+	printf("AI moving...\n");
+	dumb_ai(board);
+	}
+	
+	/*else {
+	printf("It is o's turn.\nWhere would you like to play?\n(e.g. 0 0 = top left, 1 2 = middle right)\nMake sure its number space number\nYour Move:");
+	}*/
+	
+	turn = (turn + 1) % 2;
 	}
 	
 	// Text will be printed if you won.
@@ -39,6 +58,37 @@ int main(){
 
 	// End of main function.
 	return 0;
+}
+
+void dumb_ai(char board[]){
+	int move[2];
+	//look for first open position
+	for (int r = 2; r>-1; r--){
+		for (int c = 0; c<3; c++){
+			if (board[r*12 + c*2] == ' '){
+				move[0] = r;
+				move[1] = c;
+				break;
+			}
+		}
+	}
+	printf("%d %d\n", move[0], move[1]);
+	//make the move
+	int index = move[0] * 12 + move[1] * 2;
+	board[index] = 'o';
+}
+
+void rand_ai(char board[]){
+	int move[2];
+	move[0] = rand() % 3;
+	move[1] = rand() % 3;
+	int index = move[0] * 12 + move[1] * 2;
+	while(board[index] != ' '){
+	move[0] = rand() % 3;
+	move[1] = rand() % 3;
+	index = move[0] * 12 + move[1] * 2;
+	}
+	board[index] = 'o';
 }
 
 void make_move(int turn, char board[], int move[]){
